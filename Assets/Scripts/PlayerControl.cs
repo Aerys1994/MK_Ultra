18e533e31dcf31e654fc79c58bc9327051f806ba
Aerys1994 <84088539+Aerys1994@users.noreply.github.com>
2023-08-22 20:09:59 +0200

    First commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContrl : MonoBehaviour
+{
+    public float speed, jumpHeight;
+    private float velX, velY;
+    Rigidbody2D rb;
+    public Transform groundcheck;
+    public bool isGrounded;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundcheck.position, groundCheckRadius, whatIsGround);
+        Console.WriteLine(isGrounded);
+
+        FlipCharacter();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+        Jump();
+    }
+    public void Movement()
+    {
+        velX = Input.GetAxisRaw("Horizontal");
+        velY = rb.velocity.y;
+
+        rb.velocity = new Vector2(velX * speed, velY);
+    }
+
+    public void FlipCharacter()
+    {
+        if (rb.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    public void Jump()
+    {
+        if(Input.GetButton("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+    }
+}
