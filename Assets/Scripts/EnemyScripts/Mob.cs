f1037a234da1215c0bb559b9a405fafcd26bca1c
Aerys1994 <84088539+Aerys1994@users.noreply.github.com>
2023-08-28 17:06:08 +0200

    Test_mob added
    - Added test mob with iddle animation with attack testing purposes.
    - The script with the initial parent Enemy class has been added.

---
+++ Assets/Scripts/EnemyScripts/Mob.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob : MonoBehaviour
+{
+    public string enemyName { get; protected set; }
+    public int life { get; protected set; }
+    public float speed { get; protected set; } 
+
+}
